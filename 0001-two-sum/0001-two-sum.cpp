717d06f035dd08e9ce862b8a0fb8c65f932a3fc3
anshul1822 <68806086+anshul1822@users.noreply.github.com>
2023-06-09 20:35:43 +0530

    Time: 10 ms (85.91%), Space: 10.7 MB (50.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+        //O(N) Hashing        
+        
         int n = nums.size();
         
-        vector<pair<int,int>> mp(n);
-        
+        unordered_map<int,int> mp;
+                
         for(int i=0; i<n; i++){
-            mp[i] = {nums[i], i};
-        }
-        
-        sort(mp.begin(), mp.end());
-        
-        int i = 0, j = nums.size()-1;
-        
-        while(i<j){
             
-            if(mp[i].first + mp[j].first == target) return {mp[i].second, mp[j].second};
-            else if(mp[i].first + mp[j].first < target) i++;
-            else j--;
+            int rem = target - nums[i];
+            
+            if(mp.find(rem) != mp.end()) return {i, mp[rem]};
+            
+            mp[nums[i]] = i;
         }
         
         return {-1};
+        
     }
 };
+
+// Why do we first check the presence of x - arr[i] ? 
+// We could have inserted all the elements in first go O(N) time. Then we could have parsed the input array and check the presence of x - arr[i];
+
+// Ans : X = 10 & we have 5 in arr only once
+// It will give true, in case we insert all elements before.
